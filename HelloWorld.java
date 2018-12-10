79d2378621453bcf95adc9365e55223804af954c
Kuppan, Dineshkumar <email2dineshkuppan@gmail.com>
2018-12-10 21:31:41 +0530

    Sample Java Hello World in docker container

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Dinesh on 09/12/18.
+ *
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello my old friend");
+    }
+}
